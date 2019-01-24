9ea7d4259fc3545b53bf4a7151825110da8430b4
KhallilB <khallil.bailey@gmail.com>
2019-01-24 13:33:49 -0800

    news api services routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NewsApiService } from "./services/news-api.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'news-app';
-}
+export class AppComponent {}
--- src/app/services/news-api.service.ts
+++ src/app/services/news-api.service.ts
@@ -10,9 +10,27 @@ export class NewsApiService {
 
   constructor(private _http: HttpClient) {}
 
-  initalSources() {
+  //Gets all english sources
+  getSources() {
     return this._http.get(
       "https://newsapi.org/v2/sources?language=en&apiKey=" + this.apiKey
+    );
+  }
+
+  //Gets all the top headlines in the US
+  getArticles() {
+    return this._http.get(
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + this.apiKey
+    );
+  }
+
+  //Get articles by individual sources
+  getArticlesById(source: String) {
+    return this._http.get(
+      "https://newsapi.org/v2/top-headlines?sources=" +
+        source +
+        "&apiKey=" +
+        this.apiKey
     );
   }
 }
