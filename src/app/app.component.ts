0aac213a66f3c7202864e138add5301d734b107e
KhallilB <khallil.bailey@gmail.com>
2019-01-24 18:33:42 -0800

    import material styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from "@angular/core";
-import { NewsApiService } from "./services/news-api.service";
+import { Component } from '@angular/core';
+import { NewsApiService } from './services/news-api.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
