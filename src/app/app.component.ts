02f09cba0bce79d1a83f176a5ed959a44a49afbd
KhallilB <khallil.bailey@gmail.com>
2019-01-24 18:51:11 -0800

    added basic logic to diplay news

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,28 @@ import { NewsApiService } from './services/news-api.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  mArticles: Array<any>;
+  mSources: Array<any>;
+
+  constructor(private _news: NewsApiService) {}
+
+  ngOnInit() {
+    //Loads Articles
+    this._news
+      .getArticles()
+      .subscribe(data => (this.mArticles = data['articles']));
+    //Loads Sources
+    this._news
+      .getSources()
+      .subscribe(data => (this.mSources = data['sources']));
+  }
+
+  //Searches for articles based on their id
+  searArticles(source) {
+    console.log(`Source is: ${source}`);
+    this._news
+      .getArticlesById(source)
+      .subscribe(data => (this.mArticles = data['articles']));
+  }
+}
